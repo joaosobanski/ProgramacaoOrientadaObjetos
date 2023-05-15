d7a06fc4b0dc3a4fb97e41f134aa8eed3f34d0ba
Joao Vitor Sobanski <joaovitorsobanski@Sobanski.local>
2023-05-15 19:33:22 -0300

    ex 2 listagem de animais

---
+++ Abstracao/Enum/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        Level h = Level.HIGH;
+        Level l = Level.LOW;
+
+        System.out.println(h);
+        System.out.println(l);
+    }
+}
